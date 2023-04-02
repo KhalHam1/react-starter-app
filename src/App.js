08791afb424e9bb6f36b66ac97c1d38e2a729d88
Khaleel Hamid <khaleel@lynk-me.com>
2023-04-01 23:39:09 -0400

    Added an extra line of text.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
+  //This is jsx. ie: html code in javascript coutesy react.
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+//This is the file that is executed initially when the program is run (Always).
 
 import './index.css';
 import App from './App';
