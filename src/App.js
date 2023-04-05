c2ca3bddd4312ad852a17ed9ae5e2548c6ccde94
Khaleel Hamid <khaleel@lynk-me.com>
2023-04-05 19:03:09 -0400

    The date logic has been separated from ExpenseItem and placed in its own ExpenseDate component

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
       <h2>Let's get started!</h2>
       {/* Elements starting with an Uppercase character
       are understood to be developer-made elements */}
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
     </div>
     //Now, after using the attributes to get the desired data into the components, we must go into the component and make use of the received data
     //in order to display it.
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,25 +1,10 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
-//We can use a parameter in the component function (props) to collect all the parameters received in the JSX in App.js
-//props is an object which holds all the values we get from the tag attributes in App.js
-//The data in props will be in the form of key,value pairs. Keys are the attribute name (ie title, amount and date). Values will of course be the values set.
 function ExpenseItem(props) {
-    //HARDCODED javascript no longer needed here.
-    // const expenseDate = new Date(2021, 2, 28);
-    // const expenseTitle = 'Car Insurance'; 
-    // const expensePrice = 421.00;
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.date.getFullYear();
     return (
-        // Remember to always have a single root element. IE: This div below in which all other elements are contained.
         <div className='expense-item'>
-            <div>
-                {/* {props.date.toISOString()} */}
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'> 
                 <h3>{props.title}</h3>
                 <div className='expense-item__price'>${props.amount}</div>   
@@ -27,8 +12,10 @@ function ExpenseItem(props) {
         </div>
     )
 }
-//Props are essential in having reusable components in react. It allows us to take in external values outside of the component and use it in the
-//execution of the component.
+//KEEP TRACK OF HOW THE DATE IS PASSED ON AS A PROP FROM App.js THEN TO ExpenseItem.js AND FINALLY TO ExpenseDate.js.
+//Props can be used to send data through many components.
+//The ExpenseDate component is now nested inside the ExpenseItem component.
+//Thus, the component tree takes shape.
 
 
 export default ExpenseItem;
