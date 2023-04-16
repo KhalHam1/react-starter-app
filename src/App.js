32d604a39a5334eb318bce65faa5a9af4bbe70e1
Khaleel Hamid <khaleel@lynk-me.com>
2023-04-16 07:29:38 -0400

    Explored how JSX code used to work with import React

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Expenses from "./Components/Expenses";
 
 function App() {
@@ -8,6 +9,20 @@ function App() {
     {title: 'New Desk (Wooden)', amount: 450, date: new Date(2021, 5, 12),
     }
   ];
+
+  //This is the 'underthehood' code which gets created every time I run JSX like the code that is used below.
+  //This is the way it was written with REACT in the past, and many projects still use this method.
+  //If this method is used, the 'import React from "react" statement must be used in ALL components.'
+  //In more modern React projects, this can be omitted for the simpler JSX code.
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's Get Started!"),
+  //   React.createElement(Expenses, {data: expenses})
+  // );
+  //This should clearly show as well why a root element is always needed. 
+  //It's so that the JSX code can accurately be ported over to the React.createElement method.
+  
   return (
     <div>
       <h2>Let's get started!</h2>
