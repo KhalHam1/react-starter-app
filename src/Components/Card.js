a04272bb7caba0d48624f4be948116991fc4f0b7
Khaleel Hamid <khaleel@lynk-me.com>
2023-04-16 06:56:51 -0400

    Added a container(CSS) component (Card.js)

---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+import './Card.css'
+
+function Card(props) {
+    const classes = 'card ' + props.className;
+    return <div className={classes}>{props.children}</div>//props.children is a built in prop in every React component.
+    //Children is a reserved props value. I do not set it.
+    //The value of props.children is always be the content between the opening and closing tag of my custom component.
+}
+
+export default Card;
+//This component is simply used as a CONTAINER component. 
+//It will reduce the duplication of css by executing all standard styles at once to any other component.
+//We placed all the common css from expenses and expenseItem in a separate card component, so that we don't have to duplicate the css
+//in both components.
--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,15 +1,17 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-    return (
-        <div className='expense-item'>
+    return (//In order for className='expense-item' to work, I must define it in the Card component. JSX elements like <Card> can't automatically 
+            //use css classes like html elements. I must define it as shown in the Card.js file in the 'classes' array.
+        <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'> 
                 <h3>{props.title}</h3>
                 <div className='expense-item__price'>${props.amount}</div>   
             </div>   
-        </div>
+        </Card>
     )
 }
 
