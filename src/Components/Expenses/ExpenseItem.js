9230f117a7f2c136a3a6682617b5a30f026d7dcf
Khaleel Hamid <khaleel@lynk-me.com>
2023-05-09 09:59:42 -0400

    Added a simple console log to an event handler on a temporary button

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -4,15 +4,30 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-    return (//In order for className='expense-item' to work, I must define it in the Card component. JSX elements like <Card> can't automatically 
-            //use css classes like html elements. I must define it as shown in the Card.js file in the 'classes' array.
+    const clickHandler = () => { //It is a common and sensible convention to end all function names for events with the word 'Handler'
+        console.log('Clicked!');//Check console in the web for the console log results!
+    };
+
+    return (
+        //The goal with the button is simple. Change the title(originally displayed as props.title) when clicked.
+        //Go to https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement
+        //All those events have a prop equivalent in React
+        //All prop equivalents begin with 'on' Example: 'onClick'
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'> 
                 <h3>{props.title}</h3>
                 <div className='expense-item__price'>${props.amount}</div>   
             </div>   
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
+        //All the event handler (or 'on' props like onClick need a function as a value)
+        //I generally want to define these functions before we return the component.
+        //Remember, don't execute the function in onClick like clickHandler()!, only point to it (without brackets) like clickHandler!
+
+        //IMPORTANT: Event listeners like onClick (and the others) are available on all the jsx elements (like div, h3, button, etc)
+        //Some are specific to certain elements.
+
     )
 }
 
