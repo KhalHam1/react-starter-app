fcf23f4e0445d7f75ad38504e90ec5c7323468ee
Khaleel Hamid <khaleel@lynk-me.com>
2023-05-09 11:43:50 -0400

    Added notes on how components work and why values don't simply update. Updating the STATE is necessary.

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -4,25 +4,25 @@ import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
+
+    let title = props.title; //This can be the initial value of 'title' before the button is clicked.
+                             //Can we change the value using the clickHandler function?
+
     const clickHandler = () => { //It is a common and sensible convention to end all function names for events with the word 'Handler'
         console.log('Clicked!');//Check console in the web for the console log results!
+        title = 'Updated!'; //This will not work. The title does not update. So why is that? See Expenses.js
     };
 
     return (
         //The goal with the button is simple. Change the title(originally displayed as props.title) when clicked.
-        //Go to https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement
-        //All those events have a prop equivalent in React
-        //All prop equivalents begin with 'on' Example: 'onClick'
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'> 
-                <h3>{props.title}</h3>
+                <h3>{title}</h3>
                 <div className='expense-item__price'>${props.amount}</div>   
             </div>   
             <button onClick={clickHandler}>Change Title</button>
         </Card>
-        //All the event handler (or 'on' props like onClick need a function as a value)
-        //I generally want to define these functions before we return the component.
         //Remember, don't execute the function in onClick like clickHandler()!, only point to it (without brackets) like clickHandler!
 
         //IMPORTANT: Event listeners like onClick (and the others) are available on all the jsx elements (like div, h3, button, etc)
--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -4,7 +4,9 @@ import Card from '../UI/Card'
 import './Expenses.css'
 
 function Expenses(props) {
-    return (
+    return ( //Each tag below is almost like a function call. It makes React aware of its declaration.
+             //These functions are all called only once, when the app is initialized.
+             //In order notify React that something changed and cause it to re-evaluate a component, we must utilize the concept called STATE!
         <Card className='expenses'>
         <ExpenseItem title={props.data[0].title} amount={props.data[0].amount} date={props.data[0].date}/>
         <ExpenseItem title={props.data[1].title} amount={props.data[1].amount} date={props.data[1].date}/>
